2c50e9854e42ac2ddf680824c814b456916b4319
Jake Berko <jacobberko5@gmail.com>
2024-08-08 21:56:20 -0400

    first commit

---
+++ FitnessBuddyTests/FitnessBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessBuddyTests.swift
+//  FitnessBuddyTests
+//
+//  Created by Jake Berko on 8/8/24.
+//
+
+import Testing
+@testable import FitnessBuddy
+
+struct FitnessBuddyTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
